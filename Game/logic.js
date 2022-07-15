663e4436da91c4d5616a86c7980c86d45f8bca1a
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-07-15 16:06:42 -0700

    Progress

--- Game/logic.js
+++ Game/logic.js
@@ -7,7 +7,7 @@ canvas.height = innerHeight;
 class Player {
     constructor() {
         this.position = {
-            x: 200,
+            x: canvas.width / 2 - this.width / 2,
             y: 200
         }
 
@@ -16,17 +16,39 @@ class Player {
             y: 0
         }
 
-        // this.image=
-        this.width = 100
-        this.height = 100
-
-
+        const image = new Image()
+        image.src = './assets/spaceship.png'
+        image.onload = () => {
+            const scale = 0.15
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+        }
     }
+
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if (this.image) {
+            c.drawImage(
+                this.image,
+                this.position.x,
+                this.position.y,
+                this.width,
+                this.height
+            );
+        }
+
     }
 }
 
 const player = new Player();
 player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    player.draw()
+}
+
+animate()
